18fe499cb050ff069bb71071b7d129c84aaaad86
ania105 <rodriguesania05@gmail.com>
2024-03-06 15:55:24 +0530

    prac 12

---
+++ p12/src/p12/natural_no.java
@@ -0,0 +1,13 @@
+package p12;
+import java.util.*;
+public class natural_no {
+public static void main (String args[]){
+        
+        int i;
+        System.out.println("The first 10 natural no: ");
+        for (i=1;i<=10;i++){
+            System.out.println(i);
+        }
+        
+    }
+}
