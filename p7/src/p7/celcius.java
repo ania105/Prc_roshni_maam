866b0effc4cb6bca93fb3eec8d0e39aeb5d814d0
ania105 <rodriguesania05@gmail.com>
2024-03-23 13:36:04 +0530

    Update celcius.java

--- p7/src/p7/celcius.java
+++ p7/src/p7/celcius.java
@@ -4,15 +4,32 @@ import java.util.*;
 
 public class celcius {
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Choose the conversion: ");
+		System.out.println("1. Fahrenheit to celcius");
+		System.out.println("2. Celcius to fahrenheit");
+		
+		int choice = sc.nextInt();
+		
+		if (choice==1) { 
 		System.out.println("Enter the value in fahrenheit: ");
-		Scanner scan = new Scanner(System.in);
-		double fahrenheit = scan.nextInt();
+	
+		double fahrenheit = sc.nextInt();
 		
-		double celcius = (fahrenheit - 32)/1.8;
+		double celcius = ((fahrenheit - 32)* 5)/9;
 		System.out.println("The value in celcius: " + celcius);
+		}
+		else if(choice == 2) {
+			System.out.println("Enter the value in Celcius: ");
+			double celcius = sc.nextInt();
+			double fahrenheit = ((celcius * 9)/5)+ 32;
+			System.out.println("The value in Fahrenheit is : "+ fahrenheit);
+		}
+		else {
+			System.out.println("Invalid Choice. ");
+		}
 		
-		
-		scan.close();
+		sc.close();
 		
 		
 		
