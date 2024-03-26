c22e40695d54eae8975c891dbcab745f949229ea
ania105 <rodriguesania05@gmail.com>
2024-03-26 15:59:15 +0530

    Update Main.java

--- p24/src/p24/Main.java
+++ p24/src/p24/Main.java
@@ -5,6 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		int m,n,sum=0;
 		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter digits: ");
 		m = sc.nextInt();
 		
 		while(m>0) {
