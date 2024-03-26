266fbfe2db0c838ea932aedd8f5b717498880140
ania105 <rodriguesania05@gmail.com>
2024-03-26 15:42:39 +0530

    prac 23

---
+++ p23/src/p23/Main.java
@@ -0,0 +1,16 @@
+package p23;
+import java.util.*;
+public class Main {
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the no : ");
+		int num = sc.nextInt();
+		int i = 1, fact = 1;
+		while(i<=num) {
+			fact = fact*i;
+			i++;
+		}
+		System.out.println("Factorial of "+ num +" is "+fact);
+		
+	}
+}
