201b2fefedf2d744b2a4c984dc0ec966e817049a
ania105 <rodriguesania05@gmail.com>
2024-03-26 15:58:04 +0530

    prac 24

---
+++ p24/src/p24/Main.java
@@ -0,0 +1,18 @@
+package p24;
+import java.util.Scanner;
+public class Main {
+
+	public static void main(String[] args) {
+		int m,n,sum=0;
+		Scanner sc = new Scanner(System.in);
+		m = sc.nextInt();
+		
+		while(m>0) {
+			n = m%10;
+			sum = sum +n;
+			m = m/10;
+		}
+		System.out.println("Sum of digits: "+ sum);
+	}
+
+}
