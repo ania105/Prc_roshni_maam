2c444ede6b942cc2781f55df8f9c0e3d7d59f60b
ania105 <rodriguesania05@gmail.com>
2024-03-29 09:36:31 +0530

    prac 29,31

--- p28/src/p28/main.java
+++ p28/src/p28/main.java
@@ -9,7 +9,7 @@ public class main {
 	catch(InvalidAgeException e) {
 		System.out.println("Caught"+e);
 	}
-	}
+}
 	public static void validateAge(int age) throws InvalidAgeException{
 		if(age < 18){
 			throw new InvalidAgeException("Age must be greater than or equal to 18.");
---
+++ p29/src/p29/Main.java
@@ -0,0 +1,34 @@
+package p29;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Thread thread1 = new Thread(new CountTask(1,5));
+		thread1.start();
+		
+		Thread thread2 = new Thread(new CountTask(6,10));
+		thread2.start();
+
+	}
+static class CountTask implements Runnable{
+	private int start, end;
+	public CountTask(int start, int end) {
+		this.start = start;
+		this.end = end;
+	}
+	@Override
+	public void run() {
+		for(int i = start; i<= end; i++) {
+			System.out.println(Thread.currentThread().getName()+ " - Count: "+ i);
+			
+			try {
+				Thread.sleep(1000);
+			}
+			catch(InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		
+	}
+}
+}
